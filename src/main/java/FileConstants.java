85730c5532ef9cb450aa5187c797704f1087141b
Mailsni <99682544+Mailsni@users.noreply.github.com>
2023-04-05 02:30:31 -0400

    Add files via upload

---
+++ src/main/java/FileConstants.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class FileConstants {
+	
+	public static final String CREDENTIALS_FILE_PATH = System.getProperty("user.dir")+"\\test-data\\Prod_Credentials.properties";
+		
+		
+		
+	}
+
